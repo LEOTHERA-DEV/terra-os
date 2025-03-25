7deb8bf948eeee14a84200758b52fb221aafd516
GVBRIELLE-N <kayleigh.kasitra@gmail.com>
2025-03-25 14:10:09 +0200

    Attempting to fix printing error Added some code for debugging

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -28,32 +28,32 @@ clear_screen:
     ret
 
 print_terraOS:
-
     cli
-    ; mov edi, 0xB8000 + (12 * 160) + (36 * 2)
     mov edi, 0xB8000
-.yee:
-    mov byte [edi], 'E'
-    add edi, 2
-    mov byte [edi], 'A'
-    add edi, 2
-    jmp .yee
 
 .k_loop:
     lodsb
-    test al, al ; issue
-    jz .k_done ; issue
 
-    mov byte [edi], al
-    mov byte [edi + 1], 0x0F
-    add edi, 2
+    mov [edi], esi
 
+    ; inc edi
+    ; mov byte [edi + 1], 0x0F
+    ; inc edi
+
+    mov [edi], al
+    inc edi
+    mov byte [edi + 1], 0x0E
+    inc edi
+
+
+    test al, al
+    jz .k_done
+    
     jmp .k_loop
 
 .k_done:
     mov edi, 0xB8000
     mov byte [edi], 'G'
-    ; jmp .k_loop
     ret
 
 message db "TerraOS", 0
