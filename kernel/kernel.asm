b4261f4102d85a5e5ad56211ba2d11b2f58a60c4
GVBRIELLE-N <kayleigh.kasitra@gmail.com>
2025-04-20 16:00:13 +0200

    Added motion for colour test

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -1,4 +1,8 @@
 [bits 32]
+
+section .data
+frame_offset dd 0
+
 section .text
 global kernel_main
 
@@ -13,7 +17,10 @@ kernel_main:
     mov esp, 0x90000
     mov ebp, esp
 
+main_loop:
     call color_test
+    call delay
+    jmp main_loop
 
 hang:
     hlt
@@ -23,11 +30,38 @@ color_test:
     mov edi, 0xA0000
     mov ecx, 320*200
     xor eax, eax
+    xor ebx, ebx
+    xor edx, edx
+
+    mov esi, [frame_offset]
 
 .fill_loop:
+    mov al, bl
+    add al, dl
+    add ax, si
+    add al, ah
+
     mov [edi], al
     inc edi
-    inc eax
+
+    inc bx
+    cmp bx, 320
+    jne .next_pixel
+    xor bx, bx
+    inc dl
+
+.next_pixel:
     loop .fill_loop
 
+    inc dword [frame_offset]
+
+    ret
+
+delay:
+    pusha
+    mov ecx, 0xFFFFFF
+.delay_loop:
+    dec ecx
+    jnz .delay_loop
+    popa
     ret
