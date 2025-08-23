c0388e992caad4d1e08601f4ab901b995f45d266
Gabi <kayleigh.kasitra@gmail.com>
2025-08-23 19:52:21 +0200

    Saving changes

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -8,15 +8,13 @@ global kernel_main
 
 kernel_main:
     cli
-    ; Segment registers
-    mov ax, 0x10
+    mov ax, 0x10            ; Segment registers
     mov ds, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
 
-    ;stack
-    mov esp, 0x90000
+    mov esp, 0x90000        ; stack
     mov ebp, esp
 
 main_loop:
@@ -29,42 +27,42 @@ hang:
     jmp hang
 
 color_test:
-    mov edi, 0xA0000 ; Start VGA graphics
+    mov edi, 0xA0000        ; Start VGA graphics
     mov ecx, 320*200
-    ; Clear Registers
-    xor eax, eax
+
+    xor eax, eax            ; Clear Registers
     xor ebx, ebx
     xor edx, edx
 
     mov esi, [frame_offset] ; Loading frame offset
 
 .fill_loop:
-    mov al, bl ; Colour = x position
-    add al, dl ; add y
-    add ax, si ; add frame offset
-    add al, ah ; add variation
+    mov al, bl              ; Colour = x position
+    add al, dl              ; add y
+    add ax, si              ; add frame offset
+    add al, ah              ; add variation
 
-    mov [edi], al ; Set colour value to pixel
-    inc edi ; move to next pixel
+    mov [edi], al           ; Set colour value to pixel
+    inc edi                 ; move to next pixel
 
-    inc bx ; move x
-    cmp bx, 320 ; end of row
+    inc bx                  ; move x
+    cmp bx, 320             ; end of row
     jne .next_pixel
-    xor bx, bx ; reset x
-    inc dl ; move to next y value
+    xor bx, bx              ; reset x
+    inc dl                  ; move to next y value
 
 .next_pixel:
     loop .fill_loop
 
-    inc dword [frame_offset] ; increment frame offset
+    inc dword [frame_offset]; increment frame offset
 
     ret
 
 delay:
-    pusha ; save registers
-    mov ecx, 0xFFFFFF ; Big counter
+    pusha                   ; save registers
+    mov ecx, 0xFFFFFF       ; Big counter
 .delay_loop:
     dec ecx
-    jnz .delay_loop ; repeat until ecx = 0
-    popa ; restore saved registers
+    jnz .delay_loop         ; repeat until ecx = 0
+    popa                    ; restore saved registers
     ret
