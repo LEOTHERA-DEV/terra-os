9aa28aa7b28da482e0ee42b669e751c0a8adecce
Gabi <kayleigh.kasitra@gmail.com>
2025-08-23 18:39:05 +0200

    Added documentation to kernal.asm

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -8,12 +8,14 @@ global kernel_main
 
 kernel_main:
     cli
+    ; Segment registers
     mov ax, 0x10
     mov ds, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
 
+    ;stack
     mov esp, 0x90000
     mov ebp, esp
 
@@ -27,41 +29,42 @@ hang:
     jmp hang
 
 color_test:
-    mov edi, 0xA0000
+    mov edi, 0xA0000 ; Start VGA graphics
     mov ecx, 320*200
+    ; Clear Registers
     xor eax, eax
     xor ebx, ebx
     xor edx, edx
 
-    mov esi, [frame_offset]
+    mov esi, [frame_offset] ; Loading frame offset
 
 .fill_loop:
-    mov al, bl
-    add al, dl
-    add ax, si
-    add al, ah
+    mov al, bl ; Colour = x position
+    add al, dl ; add y
+    add ax, si ; add frame offset
+    add al, ah ; add variation
 
-    mov [edi], al
-    inc edi
+    mov [edi], al ; Set colour value to pixel
+    inc edi ; move to next pixel
 
-    inc bx
-    cmp bx, 320
+    inc bx ; move x
+    cmp bx, 320 ; end of row
     jne .next_pixel
-    xor bx, bx
-    inc dl
+    xor bx, bx ; reset x
+    inc dl ; move to next y value
 
 .next_pixel:
     loop .fill_loop
 
-    inc dword [frame_offset]
+    inc dword [frame_offset] ; increment frame offset
 
     ret
 
 delay:
-    pusha
-    mov ecx, 0xFFFFFF
+    pusha ; save registers
+    mov ecx, 0xFFFFFF ; Big counter
 .delay_loop:
     dec ecx
-    jnz .delay_loop
-    popa
+    jnz .delay_loop ; repeat until ecx = 0
+    popa ; restore saved registers
     ret
